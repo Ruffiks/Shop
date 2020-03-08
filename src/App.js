64228b4cbc13db25cc3c4e607169c318e0bd36da
Ruffiks <56647098+Ruffiks@users.noreply.github.com>
2020-03-08 20:44:27 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.scss';
 function App(){
   return(
     <div>
-    <header>
-    <h1>Ruffiks's company</h1>
-    <p>Пошив Одежды</p>
-    <nav>
+    <header>    {/* header */}
+  <h1>Ruffiks's company</h1> {/* Name of company */}
+    <p>Пошив Одежды</p>       
+    <nav>     {/* navigation bar  */}
     <ul>
     <li><a href="/">Главная</a></li>
     <li><a href="/">О компании</a></li>
@@ -17,20 +17,20 @@ function App(){
     </ul>
     </nav>
     </header>
-    <main>
+    <main>    {/* main content */}
     <h2>Наши услуги</h2>
     <img src="https://html5book.ru/wp-content/uploads/2015/10/black-dress.jpg" alt="product"/>
     <h3>Маленькое черное платье</h3>
       <span className="price">₽ 1999</span>
       <a href="" className="button">В корзину</a>
-      <img src="https://html5book.ru/wp-content/uploads/2015/10/black-dress.jpg" alt="product"/>
+  <img src="https://html5book.ru/wp-content/uploads/2015/10/black-dress.jpg" alt="product"/>        {/* products */}
     <h3>Маленькое черное платье</h3>
       <span className="price">₽ 1999</span>
       <a href="" className="button">В корзину</a>
-      <img src="https://html5book.ru/wp-content/uploads/2015/10/black-dress.jpg" alt="product"/>
+  <img src="https://html5book.ru/wp-content/uploads/2015/10/black-dress.jpg" alt="product"/>        
     <h3>Маленькое черное платье</h3>
       <span className="price">₽ 1999</span>
-      <a href="" className="button">В корзину</a>
+      <a href="" className="button">В корзину</a>     {/* in the stash */}
     </main>
     </div>
   );
